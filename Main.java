0c85b9463739fda349e1e07bb11dbf0f82ec3943
RachanaBomma <31839954+RachanaBomma@users.noreply.github.com>
2017-09-12 13:03:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		StudentGroup ob1=new studentGroup();
+
+       StudentGroup ob1=new ob1();
+	
+	ob1.getStudents();
+	
+
+
+	ob1.setStudents(Student[] rachana);
+	
+
+	
+	ob1.getStudent(int 3);
+
+	
+	ob1.setStudent(Student akhila, int 2);
+
+	ob1.add(Student madhu, int 1);
+
+	
+
+	ob1.addFirst(Student ram);
+	
+
+	
+	ob1.addLast(Student raju);
+	
+	ob1.remove(int 4);
+	
+
+	
+	 
+	ob1.remove(Student madhu);
+
+	
+
+	
+}
+
+
+
